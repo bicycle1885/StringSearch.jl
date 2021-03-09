2b197ec9ebef7dbac8bdb7962cb22369c47da79e
Kenta Sato <bicycle1885@gmail.com>
2021-03-10 07:54:20 +0900

    reorganize

--- src/StringSearch.jl
+++ src/StringSearch.jl
@@ -2,8 +2,6 @@ module StringSearch
 
 const Str = Union{String,SubString{String}}
 
-const libstrsearch = joinpath(@__DIR__, "libstrsearch.so")
-
 function findnext(a::Str, b::Str, i::Int)
     if i > lastindex(b) + 1
         return nothing
@@ -20,13 +18,62 @@ function findnext(a::Str, b::Str, i::Int)
     end
 end
 
-sse2_search(a, b) =
+findnext(a::Str, b::Str, i::Integer) = findnext(a, b, Int(i))
+
+findfirst(a::Str, b::Str) = findnext(a, b, firstindex(b))
+
+const libstrsearch = joinpath(@__DIR__, "libstrsearch.so")
+
+sse2_search_cxx(a, b) =
     GC.@preserve a b Int(ccall((:sse2_search, libstrsearch), Cssize_t, (Ptr{UInt8}, Cssize_t, Ptr{UInt8}, Cssize_t), a, sizeof(a), b, sizeof(b)))
 
-avx_search(a, b) =
+avx_search_cxx(a, b) =
     GC.@preserve a b Int(ccall((:avx_search, libstrsearch), Cssize_t, (Ptr{UInt8}, Cssize_t, Ptr{UInt8}, Cssize_t), a, sizeof(a), b, sizeof(b)))
 
+function sse2_search_julia(a, b)
+    m = sizeof(a)
+    n = sizeof(b)
+    if m == 0
+        return 0
+    elseif m == 1
+        p = memchr(pointer(b), codeunit(a, 1), n)
+        return p == C_NULL ? -1 : Int(p - pointer(b))
+    end
+    F = set1_epi8(codeunit(a, 1))
+    L = set1_epi8(codeunit(a, sizeof(a)))
+    i = 0
+    while i < n - m - 14
+        S = loadu_si128(pointer(b) + i)
+        T = loadu_si128(pointer(b) + i + m - 1)
+        mask = movemask_epi8(and_si128(cmpeq_epi8(S, F), cmpeq_epi8(T, L)))
+        while mask ≠ 0
+            offset = trailing_zeros(mask)
+            if memcmp(pointer(a) + 1, pointer(b) + i + offset + 1, m - 2) == 0
+                return i + offset
+            end
+            mask &= mask - 1
+        end
+        i += 16
+    end
+    while i < n - m + 1
+        if memcmp(pointer(a), pointer(b) + i, m) == 0
+            return i
+        end
+        i += 1
+    end
+    return -1
+end
+
+# Low-level code
+# --------------
+
+memcmp(p, q, n) = ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t), p, q, n)
+memchr(p, c, n) = ccall(:memchr, Ptr{UInt8}, (Ptr{UInt8}, Cint, Csize_t), p, c, n)
+
 const m128i = NTuple{16,VecElement{UInt8}}
+
+# For debugging
+v2s(x::m128i) = String([Char(b.value) for b in x])
 
 function set1_epi8(x::UInt8)
     Base.llvmcall("""
@@ -67,48 +114,5 @@ function movemask_epi8(x::m128i)
     ret i32 %4
     """, Int32, Tuple{m128i}, x)
 end
-
-memcmp(p, q, n) = ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t), p, q, n)
-memchr(p, c, n) = ccall(:memchr, Ptr{UInt8}, (Ptr{UInt8}, Cint, Csize_t), p, c, n)
-
-function sse2_search_julia(a, b)
-    m = sizeof(a)
-    n = sizeof(b)
-    if m == 0
-        return 0
-    elseif m == 1
-        p = memchr(pointer(b), codeunit(a, 1), n)
-        return p == C_NULL ? -1 : Int(p - pointer(b))
-    end
-    F = set1_epi8(codeunit(a, 1))
-    L = set1_epi8(codeunit(a, sizeof(a)))
-    i = 0
-    while i < n - m - 14
-        S = loadu_si128(pointer(b) + i)
-        T = loadu_si128(pointer(b) + i + m - 1)
-        mask = movemask_epi8(and_si128(cmpeq_epi8(S, F), cmpeq_epi8(T, L)))
-        while mask ≠ 0
-            offset = trailing_zeros(mask)
-            if memcmp(pointer(a) + 1, pointer(b) + i + offset + 1, m - 2) == 0
-                return i + offset
-            end
-            mask &= mask - 1
-        end
-        i += 16
-    end
-    while i < n - m + 1
-        if memcmp(pointer(a), pointer(b) + i, m) == 0
-            return i
-        end
-        i += 1
-    end
-    return -1
-end
-
-v2s(x::m128i) = String([Char(b.value) for b in x])
-
-findnext(a::Str, b::Str, i::Integer) = findnext(a, b, Int(i))
-
-findfirst(a::Str, b::Str) = findnext(a, b, firstindex(b))
 
 end

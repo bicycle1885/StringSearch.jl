b43f50a1e4d39bee2bf17a23a06614baa6992075
Kenta Sato <bicycle1885@gmail.com>
2021-03-10 14:41:20 +0900

    refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,58 +1,56 @@
 using StringSearch: StringSearch, findfirst, findnext
 using Test
 
-const SS = StringSearch
-
 @testset "StringSearch.jl" begin
     b = "abracadabra"  # 11 bytes
-    @test SS.findfirst("", b) == 1:0
-    @test SS.findfirst("a", b) == 1:1
-    @test SS.findfirst("b", b) == 2:2
-    @test SS.findfirst("r", b) == 3:3
-    @test SS.findfirst("ra", b) == 3:4
-    @test SS.findfirst("abra", b) == 1:4
-    @test SS.findfirst("abracadabra", b) == 1:11
-    @test SS.findnext("abra", b, 1) == 1:4
-    @test SS.findnext("abra", b, 2) == 8:11
+    @test findfirst("", b) == 1:0
+    @test findfirst("a", b) == 1:1
+    @test findfirst("b", b) == 2:2
+    @test findfirst("r", b) == 3:3
+    @test findfirst("ra", b) == 3:4
+    @test findfirst("abra", b) == 1:4
+    @test findfirst("abracadabra", b) == 1:11
+    @test findnext("abra", b, 1) == 1:4
+    @test findnext("abra", b, 2) == 8:11
 
     b = "abracadabraabracadabra"  # 22 bytes
-    @test SS.findfirst("", b) == 1:0
-    @test SS.findfirst("a", b) == 1:1
-    @test SS.findfirst("b", b) == 2:2
-    @test SS.findfirst("r", b) == 3:3
-    @test SS.findfirst("ra", b) == 3:4
-    @test SS.findfirst("abra", b) == 1:4
-    @test SS.findfirst("abracadabra", b) == 1:11
-    @test SS.findnext("abra", b, 1) == 1:4
-    @test SS.findnext("abra", b, 2) == 8:11
+    @test findfirst("", b) == 1:0
+    @test findfirst("a", b) == 1:1
+    @test findfirst("b", b) == 2:2
+    @test findfirst("r", b) == 3:3
+    @test findfirst("ra", b) == 3:4
+    @test findfirst("abra", b) == 1:4
+    @test findfirst("abracadabra", b) == 1:11
+    @test findnext("abra", b, 1) == 1:4
+    @test findnext("abra", b, 2) == 8:11
 
     b = """
     Julia is a high-level, high-performance dynamic language for technical computing. The main homepage for Julia can be found at julialang.org. This is the GitHub repository of Julia source code, including instructions for compiling and installing Julia, below.
     """
     for a in ["a", ".", "be", "language", "code", "Julia", "installing"]
-        @test SS.findfirst(a, b) == Base.findfirst(a, b)
+        @test findfirst(a, b) == Base.findfirst(a, b)
     end
 
-    @test SS.findfirst("Julia", "Julia言語") === 1:5
-    @test SS.findnext("Julia", "Julia言語", 2) === nothing
-    @test SS.findfirst("言語", "Julia言語") === 6:9
-    @test SS.findnext("言語", "Julia言語", 2) === 6:9
-    @test SS.findnext("言語", "Julia言語", 6) === 6:9
-    @test SS.findnext("言語", "Julia言語", 7) === nothing
+    @test findfirst("Julia", "Julia言語") === 1:5
+    @test findnext("Julia", "Julia言語", 2) === nothing
+    @test findfirst("言語", "Julia言語") === 6:9
+    @test findnext("言語", "Julia言語", 2) === 6:9
+    @test findnext("言語", "Julia言語", 6) === 6:9
+    @test findnext("言語", "Julia言語", 7) === nothing
 
-    @test SS.findfirst("", "") === 1:0
-    @test SS.findnext("", "", -1) === 1:0
-    @test SS.findnext("", "", 0) === 1:0
-    @test SS.findnext("", "", 1) === 1:0
-    @test SS.findnext("", "", 2) === nothing
-    @test SS.findnext("", "", 3) === nothing
+    @test findfirst("", "") === 1:0
+    @test findnext("", "", -1) === 1:0
+    @test findnext("", "", 0) === 1:0
+    @test findnext("", "", 1) === 1:0
+    @test findnext("", "", 2) === nothing
+    @test findnext("", "", 3) === nothing
 
-    @test SS.findfirst("", "a") === 1:0
-    @test SS.findnext("", "a", -1) === 1:0
-    @test SS.findnext("", "a", 0) === 1:0
-    @test SS.findnext("", "a", 1) === 1:0
-    @test SS.findnext("", "a", 2) === 2:1
-    @test SS.findnext("", "a", 3) === nothing
+    @test findfirst("", "a") === 1:0
+    @test findnext("", "a", -1) === 1:0
+    @test findnext("", "a", 0) === 1:0
+    @test findnext("", "a", 1) === 1:0
+    @test findnext("", "a", 2) === 2:1
+    @test findnext("", "a", 3) === nothing
 end
 
 @testset "Base tests" begin
